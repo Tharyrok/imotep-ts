50941c087f2a63a640d2b73ae9b2f6a558fc267f
Tharyrok <dev@tharyrok.eu>
2016-06-11 02:53:56 +0200

    Correction stockage du mots de passe Changement de methode pour la boucle de l'event

--- hello_bot.py
+++ hello_bot.py
@@ -2,34 +2,38 @@
 
 import time
 import ts3
-import pprint
-import * from passwd.py
 
 groups = { 'general': 13, 'visitor': 8, 'host': 22, 'overwatch': 51 }
 
 
-with ts3.query.TS3Connection("ts.imotep-gaming.fr") as ts3conn:
-		ts3conn.login(
-			client_login_name=login,
-			client_login_password=password
-		)
-		ts3conn.use(sid=1)
+with ts3.query.TS3Connection("195.154.68.61") as ts3conn:
+	ts3conn.login(
+		client_login_name="YOUR LOGIN",
+		client_login_password="YOUR PASSWORD"
+	)
+	ts3conn.use(sid=1)
 
-		# Register for events
-		ts3conn.servernotifyregister(event="server")
+	# Register for events
+	ts3conn.servernotifyregister(event="server")
+	ts3conn.clientupdate(client_nickname="Robot de la team2")
 
-		while True:
-				event = ts3conn.wait_for_event()
-
-				# Greet new clients.
-				if event[0]["reasonid"] is "0":
+	while True:
+		ts3conn.send_keepalive()
+		try:
+			event = ts3conn.wait_for_event(timeout=150)
+		except ts3.query.TS3TimeoutError:
+			pass
+		else:
+			if str("reasonid") in event[0]:
+				if event[0]["reasonid"] is str('0'):
 					if event[0]["client_servergroups"] in str(groups['visitor']) :
 						ts3conn.clientmove(clid=event[0]["clid"], cid=10)
+						ts3conn.sendtextmessage(targetmode=3, target=event[0]["clid"], msg="Bonjour, merci de patienter le temps qu'un responsable vienne vous voir.")
 						for client in ts3conn.clientlist(groups=True):
-							if client['client_type'] is '0':
-								if str(groups['general']) in client['client_servergroups'] :
-									ts3conn.sendtextmessage(targetmode=1, target=client["clid"], msg="Hey il y a %s qui viens d'arriver comme visteur" % event[0]["clid"])
-								elif str(groups['host']) in client['client_servergroups'] :
-									ts3conn.sendtextmessage(targetmode=1, target=client["clid"], msg="Hey il y a %s qui viens d'arriver comme visteur" % event[0]["clid"])
-								elif str(groups['overwatch']) in client['client_servergroups'] :
-									ts3conn.sendtextmessage(targetmode=1, target=client["clid"], msg="Hey il y a %s qui viens d'arriver comme visteur" % event[0]["clid"])
+							if client['client_type'] is str('0'):
+								if str(groups['general']) in client['client_servergroups']:
+									ts3conn.sendtextmessage(targetmode=1, target=client["clid"], msg="Hey il y a %s qui viens d'arriver comme visteur" % event[0]["client_nickname"])
+								elif str(groups['host']) in client['client_servergroups']:
+									ts3conn.sendtextmessage(targetmode=1, target=client["clid"], msg="Hey il y a %s qui viens d'arriver comme visteur" % event[0]["client_nickname"])
+								elif str(groups['overwatch']) in client['client_servergroups']:
+									ts3conn.sendtextmessage(targetmode=1, target=client["clid"], msg="Hey il y a %s qui viens d'arriver comme visteur" % event[0]["client_nickname"])
